88f3dd2eb58055724194a23377fd0ce907c30e85
Makson <maksonsantoss4@gmail.com>
2023-02-09 10:23:03 -0300

    finalizada tela de login responsiva

--- src/components/Form/Input.tsx
+++ src/components/Form/Input.tsx
@@ -8,36 +8,90 @@ import {
   InputGroup,
 } from "@chakra-ui/react";
 import { FieldError } from "react-hook-form";
-import { useState, useEffect, useCallback, useRef } from "react";
+import {
+  useState,
+  useEffect,
+  useCallback,
+  ForwardRefRenderFunction,
+  forwardRef,
+} from "react";
 import { IconType } from "react-icons/lib";
 
 interface IInputProps extends ChakraInputProps {
   name: string;
   label?: string;
-  error?: FieldError | null;
+  error?:
+    | FieldError
+    | Partial<{
+        type: string | number;
+        message: string;
+      }>
+    | undefined;
   icon?: IconType;
 }
 
-export const Input = ({
-  name,
-  icon: Icon,
-  label,
-  error = null,
-  ...rest
-}: IInputProps) => {
+type inputVariationOptions = {
+  [key: string]: string;
+};
+
+const inputVariation: inputVariationOptions = {
+  error: "red.500",
+  default: "gray.200",
+  focus: "purple.800",
+  filled: "green.600",
+};
+
+const InputBase: ForwardRefRenderFunction<HTMLInputElement, IInputProps> = (
+  { name, icon: Icon, label, error = undefined, ...rest },
+  ref
+) => {
+  const [variation, setVariation] = useState("default");
+
+  const [value, setValue] = useState("");
+
+  useEffect(() => {
+    if (error) {
+      return setVariation("error");
+    }
+  }, [error]);
+
+  const handleInputFocus = useCallback(() => {
+    if (!error) {
+      setVariation("focus");
+    }
+  }, [error]);
+
+  const handleInputBlur = useCallback(() => {
+    if (value.length > 1 && !error) {
+      return setVariation("filled");
+    }
+  }, [error, value]);
+
   return (
-    <FormControl>
-      {!!label && <FormLabel>Label</FormLabel>}
+    <FormControl isInvalid={!!error}>
+      {!!label && <FormLabel>{label}</FormLabel>}
 
       <InputGroup flexDirection="column">
-        {Icon && <InputLeftElement mt="2.5" children={<Icon />} />}
+        {Icon && (
+          <InputLeftElement
+            color={inputVariation[variation]}
+            mt="2.5"
+            children={<Icon />}
+          />
+        )}
 
         <ChakraInput
           name={name}
           bg="gray.50"
+          color={inputVariation[variation]}
+          borderColor={inputVariation[variation]}
+          onFocus={handleInputFocus}
+          onBlurCapture={handleInputBlur}
+          onChangeCapture={(e) => setValue(e.currentTarget.value)}
           variant="outline"
           _hover={{ bgColor: "gray.100" }}
           _placeholder={{ color: "gray.300" }}
+          ref={ref}
           size="lg"
           h="60px"
           {...rest}
@@ -48,3 +102,5 @@ export const Input = ({
     </FormControl>
   );
 };
+
+export const Input = forwardRef(InputBase);
--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,50 +1,142 @@
-import { Flex, Grid, Heading, Image, Text, VStack } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  Flex,
+  Grid,
+  Heading,
+  Image,
+  Text,
+  VStack,
+} from "@chakra-ui/react";
 import logoBgWhite from "../../assets/logo-bg-white.svg";
 import { Input } from "../../components/Form/Input";
 import { FaEnvelope, FaLock } from "react-icons/fa";
+import { useForm } from "react-hook-form";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { useState } from "react";
 
-export const Login = () => (
-  <Flex
-    padding="10px 15px"
-    alignItems="center"
-    height="100vh"
-    bgGradient="linear(to-r, purple.800 65%, white 35%)"
-    color="white"
-  >
+const signInSchema = yup.object().shape({
+  email: yup.string().required("Email obrigatório").email("Email inválido"),
+  password: yup.string().required("Senha obrigatória"),
+});
+
+interface ISignInData {
+  email: string;
+  password: string;
+}
+
+export const Login = () => {
+  const [loading, setLoading] = useState(false);
+
+  const {
+    formState: { errors },
+    register,
+    handleSubmit,
+  } = useForm({
+    resolver: yupResolver(signInSchema),
+  });
+
+  const handleSignIn: any = (data: ISignInData) => {
+    console.log(data);
+  };
+
+  return (
     <Flex
-      w="100%"
-      justifyContent="center"
-      flexDirection="row"
+      padding={["10px 15px", "10px 15px", "0px", "0px"]}
       alignItems="center"
+      justifyContent="center"
+      height={["auto", "auto", "100vh", "100vh"]}
+      bgGradient={[
+        "linear(to-b, purple.800 65%, white 35%)",
+        "linear(to-b, purple.800 65%, white 35%)",
+        "linear(to-r, purple.800 65%, white 35%)",
+        "linear(to-r, purple.800 65%, white 35%)",
+      ]}
+      color="white"
     >
-      <Grid w="100%" paddingRight="100px">
-        <Image src={logoBgWhite} alt="do-it" boxSize="120px" />
-        <Heading as="h1">O jeito fácil, grátis</Heading>
-        <Text>
-          Flexível e atrativo de gerenciar
-          <b>seus projetos em uma única plataforma</b>
-        </Text>
-      </Grid>
-      <Grid
-        as="form"
-        mt="4"
-        w="100%"
-        padding="30px 15px"
-        border="3px solid"
-        borderColor="gray.100"
-        bg="white"
-        color="gray.900"
+      <Flex
+        w={["100%", "100%", "90%", "65%"]}
+        justifyContent="center"
+        flexDirection={["column", "column", "row", "row"]}
+        alignItems="center"
       >
-        <Heading size="lg">Bem vindo de volta!</Heading>
-        <VStack mt="6" spacing="5">
-          <Input
-            placeholder="Digite seu login"
-            name="email"
-            icon={FaEnvelope}
+        <Grid w={["100%", "100%", "50%", "50%"]} paddingRight="100px">
+          <Image
+            src={logoBgWhite}
+            alt="do-it"
+            boxSize={["120px", "120px", "150px", "150px"]}
           />
-          <Input placeholder="Digite sua senha" name="password" icon={FaLock} />
-        </VStack>
-      </Grid>
+          <Heading mt="4" as="h1">
+            O jeito fácil, grátis
+          </Heading>
+          <Text maxW="350px">
+            Flexível e atrativo de gerenciar
+            <b>seus projetos em uma única plataforma</b>
+          </Text>
+        </Grid>
+        <Grid
+          as="form"
+          onSubmit={handleSubmit(handleSignIn)}
+          mt={["4", "4", "0"]}
+          w={["95%", "95%", "40%", "40%"]}
+          padding="30px 15px"
+          border="3px solid"
+          borderColor="gray.100"
+          bg="white"
+          color="gray.900"
+        >
+          <Heading size="lg">Bem vindo de volta!</Heading>
+          <VStack mt="6" spacing="5">
+            <Box w="100%">
+              <Input
+                placeholder="Digite seu login"
+                icon={FaEnvelope}
+                label="Login"
+                error={errors.email}
+                {...register("email")}
+              />
+              {!errors.email && (
+                <Text ml="1" mt="1" color="gray.300">
+                  Exemplo: nome@email.com
+                </Text>
+              )}
+            </Box>
+            <Input
+              placeholder="Digite sua senha"
+              error={errors.password}
+              type="password"
+              icon={FaLock}
+              {...register("password")}
+            />
+          </VStack>
+          <VStack mt="4" spacing="5">
+            <Button
+              isLoading={loading}
+              bg="purple.800"
+              w="100%"
+              color="white"
+              h="60px"
+              borderRadius="8px"
+              _hover={{ background: "purple.900" }}
+              type="submit"
+            >
+              Entrar
+            </Button>
+            <Text color="gray.400">Ainda não possui uma conta?</Text>
+            <Button
+              bg="gray.100"
+              w="100%"
+              color="gray.300"
+              h="60px"
+              borderRadius="8px"
+              _hover={{ background: "gray.200" }}
+            >
+              Entrar
+            </Button>
+          </VStack>
+        </Grid>
+      </Flex>
     </Flex>
-  </Flex>
-);
+  );
+};
